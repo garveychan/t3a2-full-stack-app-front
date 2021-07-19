e9007322ee97193ec9407208dd5f39166cc54eaa
Garvey <hello@garvey.codes>
2021-07-19 18:21:10 +1000

    Add main pages and navigation bar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <header className="Navigation">
+            <h1>
+                <span>Murray</span>
+                <span>The Store of Not Much</span>
+            </h1>
+        </header>
+    );
+}
+
+export default NavBar;
