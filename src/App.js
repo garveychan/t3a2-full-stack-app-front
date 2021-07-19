fd163a930432bae38dc12be0a75001e9f32d86ce
Garvey <hello@garvey.codes>
2021-07-19 20:53:24 +1000

    Add navigation bar and landing check-in page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import home_logo from './images/home_logo.png';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import CheckIn from "./pages/CheckIn";
 
 function App() {
-    return (
-        // Placeholder only - to be deleted later 
-        <img src={home_logo} alt="1UP logo" 
-        style={{
-            backgroundColor: 'black',
-            padding: '3em',
-          }}/>
-    )
+  return (
+    <>
+      <div className="h-screen bg-gray-900 flex flex-col justify-center items-center text-center lg:px-8 lg:overflow-hidden">
+        <NavBar />
+        <div className="">
+          <Router>
+            <CheckIn />
+          </Router>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/CheckIn.js
@@ -0,0 +1,39 @@
+export default function CheckIn() {
+  return (
+    <div className="m-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-center lg:items-center">
+      <div className="lg:py-24">
+        <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+          <span className="block">1UP Bouldering</span>
+          <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-green-200 to-green-400 sm:pb-5">
+            Member Check-In
+          </span>
+        </h1>
+        <div className="mt-10 sm:mt-12">
+          <form action="#" className="sm:max-w-xl sm:mx-auto lg:mx-auto">
+            <div className="sm:flex">
+              <div className="min-w-0 flex-1">
+                <label htmlFor="email" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400 focus:ring-offset-gray-900"
+                />
+              </div>
+              <div className="mt-3 sm:mt-0 sm:ml-3">
+                <button
+                  type="submit"
+                  className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-green-400 to-green-600 text-white font-medium hover:from-green-600 hover:to-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400 focus:ring-offset-gray-900"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
