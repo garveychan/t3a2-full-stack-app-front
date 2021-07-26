681264154d8af29ba02a68d331e2660b29af55cf
garveycodes <hello@garvey.codes>
2021-07-26 21:43:50 +1000

    Remove history-based redirecting of dashboard and replace with catch-all route

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -77,10 +77,11 @@ export default function Dashboard({ loggedIn, profileComplete, adminAccess }) {
     url,
   };
 
+  console.log(initialPage.href)
+
   if (profileComplete || adminAccess) {
     return (
       <div className="h-screen flex overflow-hidden bg-gray-100">
-        {initialPage && <Redirect to={`${path}${initialPage.href}`} />}
         <SidebarMobile {...dashboardProps} />
         <Sidebar {...dashboardProps} />
         <div className="flex flex-col w-0 flex-1 overflow-hidden">
@@ -93,6 +94,10 @@ export default function Dashboard({ loggedIn, profileComplete, adminAccess }) {
                   <Route path={`${path}/members`} render={() => <Members />} />
                   <Route path={`${path}/profile`} render={() => <Profile />} />
                   <Route path={`${path}/billing`} render={() => <Billing />} />
+                  <Route
+                    path={path}
+                    render={() => <Redirect to={`${path}${initialPage.href}`} />}
+                  />
                 </Switch>
               </div>
             </div>
