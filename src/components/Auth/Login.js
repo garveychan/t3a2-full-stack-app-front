290e16083ca75adcc3ffa0c8bf2f96b211bd554d
garveycodes <hello@garvey.codes>
2021-08-02 22:15:34 +1000

    Tweak login responsiveness

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -30,7 +30,7 @@ export default function Login() {
 
   return (
     <div className="h-screen bg-gray-900 flex flex-col justify-center items-center text-center lg:px-8 lg:overflow-hidden">
-      <div className="m-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-center lg:items-center">
+      <div className="m-auto max-w-md w-full sm:w-auto px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-center lg:items-center">
         <div className="lg:py-24">
           <h1 className="text-4xl tracking-tight font-extrabold text-white sm:text-6xl xl:text-6xl">
             <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-green-200 to-green-400 sm:pb-5">
